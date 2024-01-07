8db4a066e8854e2a3d2d1aaa3b70a48d56c79c87
Nathan Taylor <nathantaylor.nate@gmail.com>
2024-01-07 14:56:39 -0500

    init and player controller done

---
+++ Player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+const WALK_SPEED = 4.0
+const SPRINT_SPEED = 8.0
+var speed
+
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Handle sprint.
+	speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+
+	move_and_slide()
